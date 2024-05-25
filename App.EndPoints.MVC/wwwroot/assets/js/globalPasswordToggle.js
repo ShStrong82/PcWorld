2b32d46b499e12ba5cd43f5a43863740cbfec721
shayan <shayan.rasouli95@gmail.com>
2024-05-26 02:12:59 +0330

    bugFixViews

---
+++ App.EndPoints.MVC/wwwroot/assets/js/globalPasswordToggle.js
@@ -0,0 +1,14 @@
+function togglePasswordField(passwordId, eyeIconId) {
+    var passwordField = document.getElementById(passwordId);
+    var eyeIcon = document.getElementById(eyeIconId);
+
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+        eyeIcon.classList.remove("fa-eye");
+        eyeIcon.classList.add("fa-eye-slash");
+    } else {
+        passwordField.type = "password";
+        eyeIcon.classList.remove("fa-eye-slash");
+        eyeIcon.classList.add("fa-eye");
+    }
+}
